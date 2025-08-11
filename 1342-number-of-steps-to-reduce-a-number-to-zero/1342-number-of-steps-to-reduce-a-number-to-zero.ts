11c5578031eb735c1318a3e6509d9baa3fcea3d2
Hansol Jeong <losnah24@gmail.com>
2025-08-11 19:05:56 +0900

    Time: 0 ms (100%), Space: 55.7 MB (23.7%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.ts
@@ -0,0 +1,9 @@
+function numberOfSteps(num: number): number {
+    let count = 0;
+    while (num > 0) {
+        if (num % 2 === 0) num /= 2;
+        else num--;
+        count++;
+    }
+    return count;
+};
