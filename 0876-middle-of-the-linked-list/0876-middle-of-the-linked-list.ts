bfdab64e62bddea7afc258c60d5d88f2de619f87
Hansol Jeong <losnah24@gmail.com>
2025-08-12 13:13:05 +0900

    Time: 0 ms (100%), Space: 55.2 MB (54.78%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.ts
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function middleNode(head: ListNode | null): ListNode | null {
+    let middle = head;
+    let end = head;
+    while (end !== null && end.next !== null) {
+        middle = middle.next;
+        end = end.next.next;
+    }
+    return middle;
+};
