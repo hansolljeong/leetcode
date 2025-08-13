31a04d8250a3e1bad9c12b17384a8b0de892e761
Hansol Jeong <losnah24@gmail.com>
2025-08-13 22:25:53 +0900

    Time: 19 ms (47.24%), Space: 59.6 MB (61.72%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.ts
+++ 0383-ransom-note/0383-ransom-note.ts
@@ -1,14 +1,15 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
-    let matchCount = 0;
-    const magazineArr = [...magazine];
-    for (const e of [...ransomNote]) {
-        const idx = magazineArr.indexOf(e);
-        if (idx !== -1) {
-            magazineArr.splice(idx, 1);
-            matchCount++;
-        } else {
-            return false;
-        }
+    let magazineLettersMap = new Map();
+    for (let i = 0; i < magazine.length; i++) {
+        const m = magazine[i];
+        magazineLettersMap[m] = magazineLettersMap[m] === undefined ? 1 : magazineLettersMap[m] + 1;
     }
-    return matchCount === ransomNote.length;
+
+    for (let i = 0; i < ransomNote.length; i++) {
+        const r = ransomNote[i];
+        if (!magazineLettersMap[r]) return false;
+
+        magazineLettersMap[r]--;
+    }
+    return true;
 };
