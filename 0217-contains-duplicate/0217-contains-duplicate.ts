68068a97e2a54e88fa8dbf0721f229b300fbfe50
Hansol Jeong <losnah24@gmail.com>
2025-08-14 12:19:49 +0900

    Time: 15 ms (81.85%), Space: 73.1 MB (89.87%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.ts
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -1,8 +1,3 @@
 function containsDuplicate(nums: number[]): boolean {
-    let map: Record<string, number> = {};
-    for (const num of nums) {
-        if (map[num] !== undefined) return true;
-        map[num] = 0;
-    }
-    return false;
+    return new Set(nums).size !== nums.length;
 };
