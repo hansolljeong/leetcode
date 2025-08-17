ba031a2eb742a227de4f5bab8830cb50123d4b8e
Hansol Jeong <losnah24@gmail.com>
2025-08-17 23:01:29 +0900

    Time: 15 ms (48.8%), Space: 61.4 MB (16.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,3 +1,13 @@
 function isAnagram(s: string, t: string): boolean {
-    return [...s].sort().join("") === [...t].sort().join("");
+    let map = new Map();
+    for (const c of s) {
+        map.set(c, (map.get(c) || 0) + 1);
+    }
+
+    for (const c of t) {
+        if (!map.has(c)) return false;
+        map.set(c, map.get(c) - 1);
+        if (map.get(c) === 0) map.delete(c);
+    }
+    return map.size === 0;
 };
