229b23181cb15e6c6fb7998fa0aa8ad3303d01f1
Hansol Jeong <losnah24@gmail.com>
2025-08-24 23:34:05 +0900

    Time: 1 ms (97.75%), Space: 61.9 MB (27.29%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,5 +1,4 @@
 function isPalindrome(s: string): boolean {
-    const aa = s.toLowerCase().split("").filter(e => (e.charCodeAt(0) > 96 && e.charCodeAt(0) < 123) || (e.charCodeAt(0) > 47 && e.charCodeAt(0) < 58));
-    const reverse = aa.toReversed();
-    return aa.join("") == reverse.join("");
+    const filtered = s.toLowerCase().split("").filter(e => (e.charCodeAt(0) > 96 && e.charCodeAt(0) < 123) || (e.charCodeAt(0) > 47 && e.charCodeAt(0) < 58));
+    return filtered.join("") == filtered.toReversed().join("");
 };
