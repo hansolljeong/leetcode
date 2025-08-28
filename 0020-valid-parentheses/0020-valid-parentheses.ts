00573ecc2825e7f587759a2bdd2e1a743bbf72a1
Hansol Jeong <losnah24@gmail.com>
2025-08-28 23:59:24 +0900

    Time: 1 ms (86.91%), Space: 56.8 MB (74%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.ts
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -1,10 +1,14 @@
 function isValid(s: string): boolean {
     const stack = [];
+    const map = {
+        ")": "(",
+        "}": "{",
+        "]": "["
+    };
+
     for (const bracket of s) {
-        if (stack.length === 0) {
-            stack.push(bracket);
-        } else if (stack.at(-1) === "(" && bracket === ")" || stack.at(-1) === "{" && bracket === "}" || stack.at(-1) === "[" && bracket === "]") {
-            stack.pop();
+        if (bracket in map) {
+            if (stack.length === 0 || stack.pop() !== map[bracket]) return false;
         } else {
             stack.push(bracket);
         }
