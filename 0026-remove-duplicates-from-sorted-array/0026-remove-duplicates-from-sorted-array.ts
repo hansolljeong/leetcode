4c7ce62018b67876ca4741d6856c0f963586a6e0
Hansol Jeong <losnah24@gmail.com>
2025-09-19 16:16:58 +0900

    Time: 0 ms (100%), Space: 58.1 MB (87.12%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -1,7 +1,7 @@
 function removeDuplicates(nums: number[]): number {
     let target = 1;
     for (let observer = 1; observer < nums.length; observer++) {
-        if (nums[target - 1] !== nums[observer]) {
+        if (nums[observer - 1] !== nums[observer]) {
             nums[target] = nums[observer];
             target++
         }
