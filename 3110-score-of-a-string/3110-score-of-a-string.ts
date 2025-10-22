2e9a5b12dcf182d27f6568e7620b4a8d1020ec70
Hansol Jeong <losnah24@gmail.com>
2025-10-22 10:40:51 +0900

    Time: 1 ms (58.25%), Space: 57.5 MB (71.36%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.ts
+++ 3110-score-of-a-string/3110-score-of-a-string.ts
@@ -1,8 +1,7 @@
 function scoreOfString(s: string): number {
-    const transformedToASCII = [...s].map(char => char.charCodeAt(0));
     let score = 0;
     for (let i = 1; i < s.length; i++) {
-        score += Math.abs(transformedToASCII[i] - transformedToASCII[i - 1]);
+        score += Math.abs(s.charCodeAt(i) - s.charCodeAt(i - 1));
     }
     return score;
 };
