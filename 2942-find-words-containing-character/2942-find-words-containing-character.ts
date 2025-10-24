e417af84b3f47a8cadbf8313cd36780baba89268
Hansol Jeong <losnah24@gmail.com>
2025-10-24 23:26:53 +0900

    Time: 1 ms (85.6%), Space: 58.8 MB (79.6%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.ts
@@ -0,0 +1,7 @@
+function findWordsContaining(words: string[], x: string): number[] {
+    const result = [];
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].includes(x)) result.push(i);
+    }
+    return result;
+};
