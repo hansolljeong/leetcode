fff20b19708118e8541acad791c54a4338043803
Hansol Jeong <losnah24@gmail.com>
2025-10-26 18:35:55 +0900

    Time: 1 ms (46.58%), Space: 57.9 MB (22.36%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.ts
@@ -0,0 +1,8 @@
+function differenceOfSums(n: number, m: number): number {
+    let num1 = 0, num2 = 0;
+    for (let i = 1; i <= n; i++) {
+        if (i % m !== 0) num1 += i;
+        else num2 += i;
+    }
+    return num1 - num2;
+};
