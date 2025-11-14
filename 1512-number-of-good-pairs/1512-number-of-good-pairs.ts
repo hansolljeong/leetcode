b825cbdcb0e7428bf54818acb0208cd665c04718
Hansol Jeong <losnah24@gmail.com>
2025-11-15 00:21:00 +0900

    Time: 0 ms (100%), Space: 55.6 MB (34.21%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.ts
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.ts
@@ -2,12 +2,8 @@ function numIdenticalPairs(nums: number[]): number {
     let result = 0;
     const count = {};
     for (const num of nums) {
-        if (count[num]) {
-            result += count[num];
-            count[num]++;
-        } else {
-            count[num] = 1;
-        }
+        if (count[num]) result += count[num];
+        count[num] = (count[num] || 0) + 1;
     }
     return result;
 };
