129d16763d8afc5962b03ae463098c8f7419be6c
Hansol Jeong <losnah24@gmail.com>
2025-11-26 23:02:02 +0900

    Time: 252 ms (72.71%), Space: 95.6 MB (66.25%) - LeetHub

---
+++ 2623-memoize/2623-memoize.ts
@@ -0,0 +1,26 @@
+type Fn = (...params: number[]) => number
+
+function memoize(fn: Fn): Fn {
+    const cacheMap = new Map();
+
+    return function(...args) {
+        const key = JSON.stringify(args);
+        if (cacheMap.has(key)) return cacheMap.get(key);
+        
+        const result = fn(...args);
+        cacheMap.set(key, result);
+        return result;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
