0ed16802424a378b3a8974f677a1a554155b7f0c
Hansol Jeong <losnah24@gmail.com>
2025-12-08 00:18:24 +0900

    Time: 52 ms (31.46%), Space: 59.2 MB (11.6%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.ts
@@ -0,0 +1,18 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function chunk(arr: Obj[], size: number): Obj[][] {
+    if (arr.length === 0) return [];
+    if (size >= arr.length) return [arr];
+    
+    let stack = [];
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        stack.push(arr[i]);
+        if (stack.length === size || i === arr.length - 1) {
+            result.push(stack);
+            stack = [];
+        }
+    }
+    return result;
+};
