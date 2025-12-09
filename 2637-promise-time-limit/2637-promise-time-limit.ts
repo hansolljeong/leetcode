786778ef354746b6d9fabfb031251bba11c3e699
Hansol Jeong <losnah24@gmail.com>
2025-12-09 23:58:06 +0900

    Time: 43 ms (91.04%), Space: 55.3 MB (68.66%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.ts
+++ 2637-promise-time-limit/2637-promise-time-limit.ts
@@ -3,7 +3,7 @@ type Fn = (...params: any[]) => Promise<any>;
 function timeLimit(fn: Fn, t: number): Fn {
     
     return async function(...args) {
-        const timeoutPromise = new Promise((_, reject) => {
+        const timeoutPromise = new Promise((resolve, reject) => {
             setTimeout(() => {
                 reject("Time Limit Exceeded")
             }, t);
