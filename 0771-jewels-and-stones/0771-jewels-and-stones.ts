a8f025d32be9abe176a24cc8e39e07c5db6ae855
Hansol Jeong <losnah24@gmail.com>
2025-12-20 14:37:11 +0900

    Time: 0 ms (100%), Space: 55.9 MB (52.38%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.ts
@@ -0,0 +1,7 @@
+function numJewelsInStones(jewels: string, stones: string): number {
+    let result = 0;
+    for (const stone of stones) {
+        if (jewels.includes(stone)) result++;
+    }
+    return result;
+};
