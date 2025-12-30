f81b287554377408497bfcce3295ec722d9980af
Hansol Jeong <losnah24@gmail.com>
2025-12-30 23:21:19 +0900

    Time: 48 ms (28.27%), Space: 54.8 MB (70.24%) - LeetHub

---
+++ 2726-calculator-with-method-chaining/2726-calculator-with-method-chaining.ts
@@ -0,0 +1,32 @@
+class Calculator {
+    private value: number;
+    
+    constructor(value: number) {
+        this.value = value;
+    }
+    
+    add(value: number): Calculator {
+        return new Calculator(this.value + value);
+    }
+    
+    subtract(value: number): Calculator {
+        return new Calculator(this.value - value);
+    }
+    
+    multiply(value: number): Calculator {
+        return new Calculator(this.value * value);
+    }
+    
+    divide(value: number): Calculator {
+        if (value === 0) throw new Error("Division by zero is not allowed");
+        return new Calculator(this.value / value);
+    }
+    
+    power(value: number): Calculator {
+        return new Calculator(Math.pow(this.value, value));
+    }
+    
+    getResult(): number {
+        return this.value;
+    }
+}
