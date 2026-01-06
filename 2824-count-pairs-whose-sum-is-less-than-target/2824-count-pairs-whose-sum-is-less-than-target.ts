fa5520ed3b76f53569cdb851aa2e61657acc1176
Hansol Jeong <losnah24@gmail.com>
2026-01-06 22:30:00 +0900

    Time: 0 ms (100%), Space: 57.9 MB (42.17%) - LeetHub

---
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.ts
@@ -0,0 +1,9 @@
+function countPairs(nums: number[], target: number): number {
+    let count = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] < target) count++;
+        }
+    }
+    return count;
+};
