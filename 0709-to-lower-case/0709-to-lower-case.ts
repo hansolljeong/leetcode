3c95e1a0d30fb7dda317a4cd6c72898ec4a97551
Hansol Jeong <losnah24@gmail.com>
2026-01-13 22:09:29 +0900

    Time: 1 ms (11.93%), Space: 56.3 MB (11.01%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.ts
+++ 0709-to-lower-case/0709-to-lower-case.ts
@@ -1,9 +1,12 @@
 function toLowerCase(s: string): string {
     let result = '';
     for (let i = 0; i < s.length; i++) {
-        const charCode = s.charCodeAt(i);
-        if (charCode > 64 && charCode < 97) result += String.fromCharCode(charCode + 32);
-        else result += s[i];
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+            const charCode = s.charCodeAt(i);
+            result += String.fromCharCode(charCode + 32);
+        } else {
+            result += s[i];
+        }
     }
     return result;
 };
